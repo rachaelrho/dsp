077e4b12062259fcd8418ef5bd8bd56c119fb961
rachaelrho <rachael.rho@gmail.com>
2016-03-22 21:40:56 -0400

    third question

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -13,20 +13,8 @@ def front_x(words):
 
 
 def sort_last(tuples):
-    """
-    Given a list of non-empty tuples, return a list sorted in
-    increasing order by the last element in each tuple.
-    e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
-         [(2, 2), (1, 3), (3, 4, 5), (1, 7)].
-
-    >>> sort_last([(1, 3), (3, 2), (2, 1)])
-    [(2, 1), (3, 2), (1, 3)]
-    >>> sort_last([(2, 3), (1, 2), (3, 1)])
-    [(3, 1), (1, 2), (2, 3)]
-    >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
-    [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
-    """
-    raise NotImplementedError
+    sort = sorted(tuples, key= lambda x: x[1])
+    print(sort)
 
 
 def remove_adjacent(nums):
