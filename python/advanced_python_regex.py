b9c47b713d0ce4470c46fff049e0a3d4ebbd4b0f
rachaelrho <rachael.rho@gmail.com>
2016-03-27 18:16:43 -0400

    q3

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,33 @@
-PLACE YOUR CODE HERE
+##Q1
+stringDegree = str(faculty[' degree'])
+degrees = set(faculty[' degree'])
+frequency = []
+
+for degree in degrees:
+    a = re.findall(degree, stringDegree)
+    frequency.append(degree)
+    frequency.append(len(a))
+
+##Q2
+stringTitle = str(faculty[' title'])
+titles = set(faculty[' title'])
+frequency = []
+
+for title in titles:
+    a = re.findall(title, stringTitle)
+    frequency.append(title)
+    frequency.append(len(a))
+
+##Q3
+
+import re
+
+hand = open('C:\\Users\\rrho002\\Desktop\\Personal\\Metis\\faculty.txt')
+
+for line in hand: 
+    line = line.rstrip()
+    x = re.findall('[a-zA-Z0-9]\S*@\S*[a-zA-Z]', line)
+    if len(x) > 0:
+        print x
+
+##Q4
